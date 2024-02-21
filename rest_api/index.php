0c7fbfa42e59ed27034ab94816cd4a57c5aef05e
TheerapatPooh <65160224@go.buu.ac.th>
2024-02-21 21:53:38 +0700

    add
    rest_api

---
+++ rest_api/index.php
@@ -0,0 +1,13 @@
+<?php
+     require_once("REST-API\dbconfig.php");
+     $db = new Database('localhost', 'persons', 'root', '');
+     if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        echo json_encode($db->query('SELECT * FROM persons_info'));
+     }
+     else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        echo 'This is POST';
+     }
+     else {
+        http_response_code(405);
+     }
+?>
